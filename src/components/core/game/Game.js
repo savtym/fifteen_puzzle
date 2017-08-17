be1e94b09c9030d127cec1bdd9cb7e200704b560
savtym <savtym@gmail.com>
2017-08-17 17:14:26 +0300

    [RLS] fifteen puzzle

--- src/components/core/game/Game.js
+++ src/components/core/game/Game.js
@@ -2,49 +2,174 @@ import React, { Component } from 'react';
 import Modal from './modal/Modal';
 import './Game.css';
 
-const listCell = [0, 1, 2, 3, 
-									4, 5, 6, 7, 
-									8, 9, 10, 11, 
-									12, 13, 14, 15];
-
+const listCell = [1,   2,  3,  4,
+									5,   6,  7,  8,
+									9,  10, 11, 12,
+									13, 14, 15,  0];
 
 class Game extends Component {
 
-	compareRandom(a, b) {
-	  return Math.random() - 0.5;
-	}
+  constructor(props) {
+    super(props);
 
-	createListCell() {
-		listCell.sort(this.compareRandom);
+    this.state = {
+      counter: 0,
+      time: 0,
+      hole: 15,
+      isStart: true,
+      listCell: listCell.slice()
+    };
 
-		this.listCell = listCell.map( (cell) =>
-			`<li>${ cell }</li>`
-		).join('');
+    this.incrementer = null;
 
-		return { __html: this.listCell };
-	}
+    this.handleChangeState = this.handleChangeState.bind(this);
+    this.handleContinueGame = this.handleContinueGame.bind(this);
+  }
+
+
+  handleChangeState(e) {
+
+    if (this.state.isStart || !e.target.hasAttribute('data-cell')) {
+      return false;
+    }
+
+    const id = e.target.getAttribute('data-cell');
+    const index = this.state.listCell.indexOf(parseInt(id, 10));
+
+    if (index < 0) {
+      return false;
+    }
+
+    const emptyI = Math.floor(this.state.hole / 4);
+    const emptyJ = this.state.hole % 4;
+    const selectedI = Math.floor(index / 4);
+    const selectedJ = index % 4;
+
+    if ((Math.abs(emptyI - selectedI) === 1 && emptyJ === selectedJ) ||
+      (Math.abs(emptyJ - selectedJ) === 1 && emptyI === selectedI)) {
+
+      [this.state.listCell[index], this.state.listCell[this.state.hole]] = [this.state.listCell[this.state.hole], this.state.listCell[index]];
+
+      this.setState(prevState => ({
+        hole: index,
+        counter: ++prevState.counter
+      }));
+
+      if (this.isCompleted()) {
+        clearInterval(this.incrementer);
+
+        const data = {
+          time: this.state.time,
+          counter: this.state.counter + 1
+        };
+
+        this._modal.addHistory(data);
+        this._modal.openModal();
+
+        this.handleContinueGame();
+      }
+
+    }
+  }
+
+
+  handleContinueGame() {
+
+    this.setState(prevState => ({
+      isStart: !prevState.isStart
+    }));
+
+    if (this.state.isStart) {
+
+      this.incrementer = setInterval( this.tick.bind(this), 1000);
+
+      this.setState(prevState => ({
+        listCell: prevState.listCell.sort(Game.compareRandom),
+        hole: prevState.listCell.indexOf(0)
+      }));
+
+
+    } else {
+
+      clearInterval(this.incrementer);
+
+      this.setState(prevState => ({
+        time: 0,
+        hole: 15,
+        counter: 0,
+        listCell: listCell.slice()
+      }));
+
+    }
+  }
+
+
+  tick() {
+    this.setState(prevState => ({
+      time: ++prevState.time
+    }));
+  }
+
+  isCompleted() {
+    for (let i = 0; i < listCell.length; i++) {
+      if (this.state.listCell[i] !== listCell[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
 
   render() {
     return (
-      <section>
+			<section>
+				<div className="container">
+					<div className="content">
+						<div className="info clearfix">
+							<span className="time"><span>Time: </span>{ Game.formattedSeconds(this.state.time) }</span>
+							<span className="counter pull-right"><span>Counter: </span>{ this.state.counter }</span>
+						</div>
 
-      	<div className="info">
-		      <span className="time">00:00</span>
-		      <span className="counter">0</span>
-		    </div>
+						<div className="game-block clearfix" onClick={ this.handleChangeState }>
+							<TableGame items={ this.state.listCell }/>
+						</div>
 
-		    <div className="game-block">
-		      <ul dangerouslySetInnerHTML={this.createListCell()} />
-		    </div>
+						<div className="buttons clearfix">
+							<button className="btn btn-primary start" onClick={ this.handleContinueGame }>{ this.state.isStart ? 'Start' : 'Stop' }</button>
+              <Modal ref={(modal) => { this._modal = modal; }} />
+						</div>
+					</div>
+				</div>
 
-		    <div className="buttons">
-		      <button className="start">Start</button>
-		      <button className="history">History</button>
-		    </div>
 
-		    <Modal />
+			</section>
+    );
+  }
 
-      </section>
+  static compareRandom() {
+    return Math.random() - 0.5;
+  }
+
+  static formattedSeconds(sec) {
+    return `${ ('0' + Math.floor(sec / 60)).slice(-2) }:${ ('0' + sec % 60).slice(-2) }`;
+  }
+}
+
+
+class TableGame extends React.Component {
+  render() {
+    return (
+			<ul>
+        {this.props.items.map(cell => {
+
+        	if (cell === 0) {
+        		return <li className="empty"></li>;
+					} else {
+            return <li data-cell={ cell }>{ cell }</li>;
+					}
+
+        })}
+			</ul>
     );
   }
 }
--- src/components/core/game/modal/Modal.js
+++ src/components/core/game/modal/Modal.js
@@ -1,12 +1,108 @@
 import React, { Component } from 'react';
+import ModalView from 'react-modal';
 import './Modal.css';
 
+
+const customStyles = {
+  content : {
+    top                   : '20%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    width                 : '480px'
+  }
+};
+
+const keyLocalStorage = 'history_fifteen_puzzle';
+
+
 class Modal extends Component {
+  constructor() {
+    super();
+
+    const data = localStorage.getItem(keyLocalStorage);
+
+    this.state = {
+      history: (data) ? JSON.parse(data) : [],
+      modalIsOpen: false
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  addHistory(data) {
+    this.state.history.push(data);
+    localStorage.setItem(keyLocalStorage, JSON.stringify(this.state.history))
+  }
+
   render() {
     return (
-      <div className="modal">
+      <div style={{display: 'inline'}}>
+        <button className="btn btn-info history pull-right" onClick={ this.openModal }>History</button>
+
+        <ModalView
+          isOpen={ this.state.modalIsOpen }
+          onAfterOpen={ this.afterOpenModal }
+          onRequestClose={ this.closeModal }
+          style={customStyles}
+          contentLabel="Modal for history game"
+        >
+          <div className="modal-content">
+            <div className="modal-header">
+              <div className="close clearfix">
+                <span className="close pull-right" onClick={ this.closeModal }>&times;</span>
+              </div>
+              <h2>History</h2>
+            </div>
+
+            <div className="modal-body">
+              <HistoryTable items={ this.state.history }/>
+            </div>
+
+          </div>
+        </ModalView>
 
       </div>
+    );
+  }
+}
+
+class HistoryTable extends React.Component {
+  render() {
+    return (
+      <table>
+        <thead>
+        <tr>
+          <th>Time</th>
+          <th>Counter</th>
+        </tr>
+        </thead>
+
+        <tbody>
+          {this.props.items.map(item =>
+            <tr>
+              <td>{ item.time }</td>
+              <td>{ item.counter }</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     );
   }
 }
